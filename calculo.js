cc18428a5abb634097766f4f2fe3d3589ac45a7e
lara <130380646+LaraBC2007@users.noreply.github.com>
2023-10-23 07:53:46 -0300

    Add files via upload

---
+++ calculo.js
@@ -0,0 +1,6 @@
+function Calcular(){
+
+    let idadeCanina = prompt("Qual a idade do cachorrinho?");
+
+ mostraResultado.textContent = "Resultado: a idade humana do cachorrinho é " + idadeCanina;
+}
